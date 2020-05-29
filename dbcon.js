d34a3695f8b59f226ee55bfcc4c0a8d44e8c145a
ngojs2 <59804517+ngojs2@users.noreply.github.com>
2020-05-28 19:19:25 -0700

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_ngojef',
-  password        : '',
-  database        : 'cs340_ngojef'
+  user            : 'username',
+  password        : 'pw',
+  database        : 'username'
 });
 
 module.exports.pool = pool;
